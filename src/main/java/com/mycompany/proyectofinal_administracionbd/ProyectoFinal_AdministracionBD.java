afac41abb404e1b5d1bfa55fa873f313ea5635eb
Nicole Bañuelos <nicole.bc@tecjerez.edu.mx>
2026-05-06 18:53:50 -0600

    Creación del proyecto

---
+++ src/main/java/com/mycompany/proyectofinal_administracionbd/ProyectoFinal_AdministracionBD.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectofinal_administracionbd;
+
+/**
+ *
+ * @author banue
+ */
+public class ProyectoFinal_AdministracionBD {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
