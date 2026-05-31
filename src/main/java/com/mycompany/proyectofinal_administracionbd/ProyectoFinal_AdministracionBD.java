0136889df0afe2ffdaa35c2a603f4255a2cb4aa7
Nicole Bañuelos <nicole.bc@tecjerez.edu.mx>
2026-05-30 23:04:14 -0600

    cambio minimo en el package raiz, añadi un import nuevo y necesario para que puediera ejecutar el programa

--- src/main/java/com/mycompany/proyectofinal_administracionbd/ProyectoFinal_AdministracionBD.java
+++ src/main/java/com/mycompany/proyectofinal_administracionbd/ProyectoFinal_AdministracionBD.java
@@ -1,3 +1,5 @@
+package com.mycompany.proyectofinal_administracionbd;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
